8bf7713febebfd2ad1598e87f86d4fa340b46f9c
tonyfan <waterwet@outlook.com>
2026-06-28 18:26:54 +0800

    sm7750-common-vendor: Add stub vendor makefiles

---
+++ BoardConfigVendor.mk
@@ -0,0 +1,2 @@
+# Stub BoardConfigVendor for sm7750-common
+# Will be populated with proprietary partition sizes in Phase 3
---
+++ sm7750-common-vendor.mk
@@ -0,0 +1,2 @@
+# Stub vendor makefile for sm7750-common
+# Will be populated with proprietary blob definitions in Phase 3
